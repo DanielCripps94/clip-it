dbb7f7a3b188fead3f38fab0c8f62511971325c2
Daniel Cripps <dan-cripps@hotmail.co.uk>
2025-01-19 12:58:44 +0000

    add: suspense

--- src/layers/2-pages/login/index.tsx
+++ src/layers/2-pages/login/index.tsx
@@ -1,5 +1,10 @@
 import { LoginFormWidget } from "@/layers/3-widgets/login-form-widget/facade";
+import { Suspense } from "react";
 
 export default function LoginPage() {
-  return <LoginFormWidget />;
+  return (
+    <Suspense>
+      <LoginFormWidget />;
+    </Suspense>
+  );
 }
