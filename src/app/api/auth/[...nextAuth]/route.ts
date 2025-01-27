a671f58ef1f8eda3fc9c21657d99a105ab283832
Daniel Cripps <dan-cripps@hotmail.co.uk>
2025-01-27 09:30:23 +0000

    add: attempt fix

--- src/app/api/auth/[...nextAuth]/route.ts
+++ src/app/api/auth/[...nextAuth]/route.ts
@@ -3,4 +3,6 @@ import { authOptions } from "~/auth-settings";
 
 const handler = NextAuth(authOptions);
 
+console.log("NextAuth route handler is being executed");
+
 export { handler as GET, handler as POST };
